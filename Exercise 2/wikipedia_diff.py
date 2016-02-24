f7eb66eec662a5e2a7232e1cc1673e52e6f77799
edoroth <enr2116@columbia.edu>
2016-02-24 00:12:46 -0500

    Version of full pipeline

--- Exercise 2/wikipedia_diff.py
+++ Exercise 2/wikipedia_diff.py
@@ -1,34 +1,60 @@
-#Author: Edo Roth
-#Storytelling with Streaming Data
-#Code for submission for Exercise 2
+# Author: Edo Roth
+# Storytelling with Streaming Data
+# Code for submission for Exercise 2
+# Modified some code from code provided in lecture on 2/10/16
 #2/24/16
 
 
 import json
 import sys
-import redis
-
 
 last = 0
+# Create empty lists to store most recent differences and most recent edits
 diffs = []
+edits = []
+
+# Run forever to ensure we continually get all messages and constantly measure rate
 while 1:
+	# Get output of previous program which is pumped in through standard input
     line = sys.stdin.readline()
+    # Parse JSON format
     d = json.loads(line)
+    # Get
     if last == 0 :
         last = d["t"]
         continue
     delta = d["t"] - last
-    #print (json.dumps({"delta":delta, "t":d["t"]}))
-    #We calculate the rate by looking at 10 messages a time
-    if (len(diffs) < 10):
+    # We calculate the rate by averaging over 3 edits at a time
+    # As long as we haven't seen 3 edits, we stored the time differences and edits themselves
+    if (len(diffs) < 3):
     	diffs.append(delta)
-    if (len(diffs) == 10):
+    	edits.append(d)
+    # We measure the rate over an average of 3 total differences. This gives us an average rate for our stream.
+    # I chose three because this is a good indication of noticeable activity in editing a specific category
+    if (len(diffs) == 3):
+    	# Calculate rate by summing all differences over the last 3 differences seen
     	rate = sum(diffs)/float(len(diffs))
-    	print ("rate is %.3f seconds per message" % rate )
-    	if (rate < 0.500):
-    		print ("Holy smokes we've got some activity!")
+    	if (rate < 120.00):
+    		s = ""
+    		i = 0
+    		# Formatting to print out the names of the most recent articles edited.
+    		# I chose to print the names of the individual articles edited, so that
+    		# when we get an uptick in rate, we can notify the specific articles edited
+    		# as evidence of activity -- this may give some insight on the reasons for
+    		# some of these relatively high-paced changes
+    		for	edit in edits:
+    			if (i != len(edits)-1):
+    				s = s + edit["name"] +  ", "
+    			else:
+    				s = s + edit["name"] + "."
+    			i = i + 1
+    		# Printing out one line for ease of converting an alert to a tweet
+    		print (s)
+    		sys.stdout.flush()
+    	# Reset lists to begin a new rate counter	
     	diffs = []
-    sys.stdout.flush()
+    	edits = []
+
     last = d["t"]
 
 
--- Exercise 2/wikipedia_stream_2.py
+++ Exercise 2/wikipedia_stream_2.py
@@ -10,53 +10,82 @@ import time
 import codecs
 from sys import stdout
 
-''' Function to remove all non-ASCII characters'''
+''' Function to remove all non-ASCII characters
+Wikipedia contains many articles with non-ASCII characters in their titles
+and for the purpose of this project these are not meaningful, as all candidates
+have only ASCII characters in their names'''
 def checkstring(s):
     good_chars = (c for c in s if 0 < ord(c) < 127)
     return ''.join(good_chars)
 
-#Creating blank dictionary to keep track of logged edits (and not print duplicates
-#if request returns same entry twice)
+# Creating blank dictionary to keep track of logged edits (and not print duplicates
+# if request returns same entry twice)
+# 
+# It's important that every single edit is marked as a separate entry, so we keep track
+# of a dictionary which is indexed by revision id. Wikipedia guaranteed unique revision
+# identificication numbers for every unique edit which we use for this purpose.
 logged_edits = {}
 
-#Run forever
+# List of major candidates still in play in the 2016 presential election
+# I chose to look at edits containing the full name of any of these candidates because these articles are most relevent
+# and contain official information about candidates  and their campaign. Choosing only last names, for instance,
+# adds a whole host of articles that may not directly pertain to these seven candidates.
+candidates = ["Donald Trump", "Hillary Clinton", "Bernie Sanders", "Ben Carson", "Ted Cruz", "Marco Rubio", "John Kasich"]
+
+# Run forever
+# Ensures that this continues to gather information for as long as the service is running
 while True:
 
-    #Send request to Wikipedia API (will get 10 changes at a time)
-    s = requests.get('https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&format=json&rcprop=ids|title|user|timestamp&rclimit=10')
+    # Send request to Wikipedia API (will get 100 changes at a time)
+    # I chose to receive the following information:
+    #   -Revision ID
+    #   -Title of Article
+    #   -Timestamp of Article's edit
+    # All of this information is essential to verify that we are dealing with unique edits, and to extract information about the articles being edited
+    s = requests.get('https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&format=json&rcprop=ids|title|timestamp&rclimit=100')
 
-    #Check status code to ensure good response
+    # Check status code to ensure good response
+    # If any code other than 200 is received, the program may crash and I wanted a tool to understand the failure.
+    # This error will likely arise only from server error, as the request is properly formatted.
     if (s.status_code != 200):
-        print ("UH OH")
+        print ("ERROR: STATUS CODE OTHER THAN 200")
 
-    #Parse into JSON format
+    # Parse into JSON format
+    # Wikipedia provides data in a JSON format and this is a useful format for us to use
     j = s.json()
 
-    #Get list of changes
+    #Get list of all recent changes by using appropriate JSON tags
     all_changes = j["query"]["recentchanges"]
 
-    #Go through all changes in received by request
+    # Go through all changes in received by request
+    # We want to comb through all recent changes to ensure we cover all edits that have been made while the program runs
     for change in all_changes:
 
-        #Check if entry has already been encountered
+        # Check if entry has already been encountered by checking its revision ID
+        # If the entry has not been seen, we add it to our dictionary to mark it
         if change["revid"] not in logged_edits:
             logged_edits[change["revid"]] = change["revid"]
 
-            #Get rid of category and talk pages -- only using articles
+            # Get rid of category and talk pages
+            # The colon only appears in titles when they are not main articles pages.
+            # As a study of articles about candidates, and the way that they are used to convey information to the public,
+            # I chose to ignore category and talk pages because they are more internally-facing articles and thus don't 
+            # reflect edits meant for view by the wider public
             if  ":" not in change["title"] :
-                #Get specific user details with additional API request
-                this_user = requests.get('https://en.wikipedia.org/w/api.php?action=query&list=users&ususers=' + change["user"] + '&format=json&usprop=editcount')
-                user = this_user.json()
 
-                #Print time and message
-                print (json.dumps({"t": time.time(), "message": checkstring(change["title"]) + " was edited "}))
+                for candidate in candidates:
+                    # Check if any of the candidates' names appear in the article title.
+                    # I chose this metric because this set of articles encompass all official
+                    # Wikipedia content pertaining to any of the presidential candidate.
+                    if (candidate in checkstring(change["title"])):
+                        # Print the title of the article that was edited along with the current time
+                        # This allows every single edit to be logged individually when it comes in.
+                        print (json.dumps({"t": time.time(), "name": checkstring(change["title"])}))
 
-                #Get rid of anonymous users by ensuring that an edit count exists
-                #if ('editcount' in user["query"]["users"][0]):
-                    #Print output to stdout
-                    #print (json.dumps({"t": time.time(), "message": checkstring(change["title"]) + " was edited "}))
         #Empty the buffer
         stdout.flush()
 
-    #Wait before next request
-    time.sleep(0.1)
+    # Wait before next request.
+    # Waiting for 5 seconds before sending a request means that we have a margin of error of 5 seconds in marking the time difference
+    # between edits. However, as we deem a string of 5 messages coming every 2 minutes as significant, this margin is insignificant.
+    time.sleep(5)
